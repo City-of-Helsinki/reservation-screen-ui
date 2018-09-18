81ab7aa05249828927b77e093e95a6af001bdf57
jaamo <jaakko@evermade.fi>
2018-09-18 13:02:52 +0300

    Prettified file

--- app/components/AreaBooking/ButtonList.js
+++ app/components/AreaBooking/ButtonList.js
@@ -3,41 +3,43 @@ import BasicButton from 'components/BasicButton';
 import styled from 'styled-components';
 
 const BookingButton = styled(BasicButton)`
-	margin: 0 auto 1rem;
-	display: block;
-	&:last-of-type {
-		margin-bottom: 3rem;
-	}
+  margin: 0 auto 1rem;
+  display: block;
+  &:last-of-type {
+    margin-bottom: 3rem;
+  }
 `;
 
 const Wrapper = styled.div`
-	text-align: center;
+  text-align: center;
 `;
 
 class ButtonList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeButton: this.props.active,
+    };
+  }
 
-	constructor(props){
-		super(props);
-		this.state = {
-			activeButton: this.props.active
-		}
-	}
+  componentDidMount(props) {}
 
-	componentDidMount(props){
-		
-	}
-
-	render() {
-	    return (
-	      <Wrapper>
-	        {this.props.items.map((item, index) => (
-	          <BookingButton id={item.id} className={item.active ? 'active' : ''} onClick={(e) => this.props.onButtonClick(e, index)} key={item.id}>
-	          	Kello {item.text} saakka
-	          </BookingButton>
-	        ))}
-	      </Wrapper>
-	    );
-	}
+  render() {
+    return (
+      <Wrapper>
+        {this.props.items.map((item, index) => (
+          <BookingButton
+            id={item.id}
+            className={item.active ? 'active' : ''}
+            onClick={e => this.props.onButtonClick(e, index)}
+            key={item.id}
+          >
+            Kello {item.text} saakka
+          </BookingButton>
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default ButtonList;
