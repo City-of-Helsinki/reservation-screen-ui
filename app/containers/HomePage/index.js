d8f3461eedc2f7d942dc2c6f8e2fc3e7f603281b
Evermade <evermade@antti.em87.io>
2018-09-18 13:02:04 +0000

    Pull

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -75,11 +75,9 @@ export class HomePage extends React.PureComponent {
 
     this.state = {
       theme: themeSoon,
+      theme: themeFree,
     };
-      theme: themeFree
-    }
   }
-
   /**
    * when initial state username is not null, submit the form to load repos
    */
