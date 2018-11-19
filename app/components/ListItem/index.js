cc7aadcbbc11e2079f00d5bf7617a88a757d90ff
jaamo <jaakko@evermade.fi>
2018-11-19 13:23:06 +0200

    Show event title on upcoming list.

--- app/components/ListItem/index.js
+++ app/components/ListItem/index.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import moment from 'moment';
 
-import Item from './Item';
 import Wrapper from './Wrapper';
+
+const Item = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  width: 100%;
+  height: 100%;
+  align-items: flex-start;
+  line-height: 46px;
+  font-size: 28px;
+`;
+
+const ItemTime = styled.div`
+  width: 40%;
+`;
+
+const ItemTitle = styled.div`
+  width: 60%;
+  overflow: hidden;
+  height: 46px;
+`;
 
 function ListItem(props) {
   return (
     <Wrapper>
       <Item>
-        {moment(props.item.get('begin')).format('HH.mm')} -{' '}
-        {moment(props.item.get('end')).format('HH.mm')}
+        <ItemTime>
+          {moment(props.item.get('begin')).format('HH.mm')} -{' '}
+          {moment(props.item.get('end')).format('HH.mm')}
+        </ItemTime>
+        <ItemTitle>{props.item.get('event_subject') || ''}</ItemTitle>
       </Item>
     </Wrapper>
   );
