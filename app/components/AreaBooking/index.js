04082f255bc5044a47d7ff2faa7c286b8ba35928
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2020-03-05 16:09:48 +0200

    Use prop-types from separate package

--- app/components/AreaBooking/index.js
+++ app/components/AreaBooking/index.js
@@ -1,5 +1,6 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 import LocaleToggle from 'containers/LocaleToggle';
+import PropTypes from 'prop-types';
 
 import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
